72241c7350a0128c510ce17529ac545cedb1a11a
Ali Shan <alishanamin125@gmail.com>
2025-04-09 10:14:49 +0500

    init

--- Task/prog01.py
+++ Task/prog01.py
@@ -2,4 +2,9 @@ class Student:
     def __init__(self): 
         print("Student instance created")
         
+    def show(self,x,y):
+        print(x,y)
 s1 = Student()
+s1.x = 10
+s1.y = 20
+s1.show(s1.x,s1.y)
