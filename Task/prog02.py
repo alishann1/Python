a71b6ef67285fdfb5088a37388c1e6e4c12e8c41
Ali Shan <alishanamin125@gmail.com>
2025-04-16 10:05:40 +0500

    prog 02

--- Task/prog02.py
+++ Task/prog02.py
@@ -1,4 +1,28 @@
-i = len("Anaconda")
-print(i)
-i = len([1,2,3,4])
-print(i)
+# i = len("Anaconda")
+# print(i)
+# i = len([1,2,3,4])
+# print(i)
+
+
+# def add (a,b,c=0):
+#     return a + b + c
+# print(add(3,4))
+# print(add(3,4,6))
+# print(add(3,4,8))
+
+class A:
+    def f(self, i = None):
+        if i == None:
+            print("Sequence 01")
+        else:
+            print("Sequence 02")
+            
+
+def main():
+    a = A()
+    a.f()
+    a.f(3)
+    
+if __name__ == "__main__":
+    main()    
+    
---
+++ Task/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+def add (a,b,c=0):
+#     return a + b + c
+# print(add(3,4))
+# print(add(3,4,6))
+# print(add(3,4,8))
