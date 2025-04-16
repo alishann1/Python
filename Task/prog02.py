3a843d4e29f7339bc814d9c2fc24609b45d36cc2
Ali Shan <alishanamin125@gmail.com>
2025-04-16 13:37:16 +0500

    prog 02

--- Task/prog02.py
+++ Task/prog02.py
@@ -10,19 +10,53 @@
 # print(add(3,4,6))
 # print(add(3,4,8))
 
-class A:
-    def f(self, i = None):
-        if i == None:
-            print("Sequence 01")
-        else:
-            print("Sequence 02")
+# class A:
+#     def f(self, i = None):
+#         if i == None:
+#             print("Sequence 01")
+#         else:
+#             print("Sequence 02")
             
 
-def main():
-    a = A()
-    a.f()
-    a.f(3)
+# class B(A):
+#     def f(self, i= None):
+#         if i == None:
+#             print("Sequence 01 in child class")
+#         else: 
+#             print("Sequence 02 in the child")
+
+# def main():
+#     a = B()
+#     a = A()
+#     a.f()
+#     a.f(3)
     
-if __name__ == "__main__":
-    main()    
+# if __name__ == "__main__":
+#     main()    \
     
+class Pet:
+    def __init__(self, name1, age1):
+        self.name1 = name1
+        self.age1 = age1
+    def __str__(self):
+        return self.name1 + " " + str(self.age1)
+class Dog(Pet):
+    def __init__(self, name1, age1, breed1):
+        super().__init__(name1, age1)
+        self.breed1 = breed1
+    def __str__(self):
+        return super().__str__() + self.breed1
+        
+    def bark(self):
+        print("bark bark")
+        
+class Cat(Pet):
+    def purr(self):
+        print("purr purr")
+        
+spotty = Dog("Spotty", 5 ," Golden Retriever")
+cutie = Cat("Cutie ", 4)
+print(spotty)
+print(cutie)
+# spotty.bark()
+# cutie.purr()
