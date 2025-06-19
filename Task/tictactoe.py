4a5a167c6684a6dea23cded66c0c2045faaf8b4a
Ali Shan <alishanamin125@gmail.com>
2025-06-19 21:32:22 +0500

    tictactoe

---
+++ Task/tictactoe.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+
+pygame.init()
+
+width = 300
+height = 300
+
+screen = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption("Tic Tac Toe")
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((255, 255, 255))
+
+    pygame.draw.line(screen, (0, 0, 0), (100, 0), (100, 300), 3)
+    pygame.draw.line(screen, (0, 0, 0), (200, 0), (200, 300), 3)
+
+    pygame.draw.line(screen, (0, 0, 0), (0, 100), (300, 100), 3)
+    pygame.draw.line(screen, (0, 0, 0), (0, 200), (300, 200), 3)
+
+    pygame.display.update()
