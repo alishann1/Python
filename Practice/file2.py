2cc1c7ddb5ac920b79bf5a5128226dc032b57feb
Ali Shan <alishanamin125@gmail.com>
2025-06-28 23:04:24 +0500

    python

--- Practice/file2.py
+++ Practice/file2.py
@@ -1,12 +1,16 @@
-# n = 1 
- 
+# n = 1
+
 # print("Please enter a number: ")
-# i = int(input())  
+# i = int(input())
 
 # while n <= 10:
-#     print(f"{i} x {n} = {i * n}") 
-#     n = n + 1  
+#     print(f"{i} x {n} = {i * n}")
+#     n = n + 1
 
-myArray = [1,2,3,4,4]
+# myArray = [1,2,3,4,4]
 
-print(myArray)
+# print(myArray)
+
+board = [["" for _ in range(3)] for _ in range(3)]
+for row in board:
+    print(row)
